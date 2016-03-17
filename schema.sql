feee9b1614e0bb0b3dca3f29a4ed5ce902569196
Mario Domgoergen <mdom@taz.de>
2016-03-17 19:18:15 +0100

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,16 +1,19 @@
 CREATE TABLE tweets (
-        id INTEGER PRIMARY KEY,
+        tweet_id INTEGER PRIMARY KEY,
         user INTEGER NOT NULL,
         tweet TEXT,
-        timestamp NOT NULL DEFAULT ( strftime('%s','now') ),
-        FOREIGN KEY(user) REFERENCES users(id),
-        CONSTRAINT tweet_user_timestamp UNIQUE ( user,tweet,timestamp ) ON CONFLICT IGNORE
+        timestamp INTEGER NOT NULL DEFAULT ( strftime('%s','now') ),
+        added     INTEGER NOT NULL DEFAULT ( strftime('%s','now') ),
+        FOREIGN KEY(user) REFERENCES users(user_id) ON DELETE CASCADE,
+        CONSTRAINT tweet_user_timestamp UNIQUE ( user,tweet,timestamp )
 );
 CREATE TABLE users (
-        id INTEGER PRIMARY KEY,
+        user_id INTEGER PRIMARY KEY,
         nick TEXT,
         url TEXT UNIQUE,
-        timestamp NOT NULL DEFAULT ( strftime('%s','now') ),
-	last_modified text,
+        timestamp INTEGER NOT NULL DEFAULT ( strftime('%s','now') ),
+	last_modified TEXT,
+	active INTEGER DEFAULT 1,
+	last_error TEXT
 );
 
